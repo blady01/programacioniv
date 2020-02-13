201f06a3dabcae1ac81f73066cdfee7afa791314
blady01 <bladimirhenriquez06@gmail.com>
2020-02-13 13:36:12 -0600

    HOLA

--- app.js
+++ app.js
@@ -10,19 +10,27 @@ document.addEventListener("DOMContentLoaded", e=>{
         console.log(codigo, nombre, direccion, telefono);
         
         if( 'localStorage' in window ){
-            window.localStorage.setItem("codigo", codigo);
-            window.localStorage.setItem("nombre", nombre);
-            window.localStorage.setItem("direccion", direccion);
-            window.localStorage.setItem("telefono", telefono);
+            window.localStorage.setItem("codigo"+codigo, codigo);
+            window.localStorage.setItem("nombre"+codigo, nombre);
+            window.localStorage.setItem("direccion"+codigo, direccion);
+            window.localStorage.setItem("telefono"+codigo, telefono);
         } else {
             alert("Por favor ACTUALIZATE!!!.");
         }
     });
     document.querySelector("#btnRecuperarAlumno").addEventListener("click", event=>{
-        document.querySelector("#txtCodigoAlumno").value = window.localStorage.getItem("codigo");
-        document.querySelector("#txtNombreAlumno").value = window.localStorage.getItem("nombre");
-        document.querySelector("#txtDireccionAlumno").value = window.localStorage.getItem("direccion");
-        document.querySelector("#txtTelefonoAlumno").value = window.localStorage.getItem("telefono");
+
+        if (document.querySelector("#txtCodigoAlumno").value = !""){
+            document.querySelector("#txtCodigoAlumno").value = window.localStorage.getItem("codigo"+document.querySelector("#txtCodigoAlumno").value);
+            document.querySelector("#txtNombreAlumno").value = window.localStorage.getItem("nombre"+document.querySelector("#txtCodigoAlumno").value);
+            document.querySelector("#txtDireccionAlumno").value = window.localStorage.getItem("direccion"+document.querySelector("#txtCodigoAlumno").value);
+            document.querySelector("#txtTelefonoAlumno").value = window.localStorage.getItem("telefono"+document.querySelector("#txtCodigoAlumno").value);
+        }
+        else if (document.querySelector("#txtCodigoAlumno").value = ""){
+            alert("Introduzca un codigo de usuario");
+        }
+
+  
     });
 });
 
