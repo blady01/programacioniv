992a29aa90e35e4cc17d985a46f9b671a4eefa86
blady01 <bladimirhenriquez06@gmail.com>
2020-03-20 18:26:53 -0600

    terminado alumnos y docentes

---
+++ private/config/config.php
@@ -0,0 +1,7 @@
+<?php 
+/**
+ * Importamos la clase principal de conexion
+ */
+include('../../conexion/DB.php');
+$conexion = new DB('localhost','root','','db_sistema_academica_prograa2');
+?>
--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,39 @@
 var $ = el => document.querySelector(el);
-document.addEventListener("DOMContentLoaded",event=>{
-    let mostrarVista = $("[class*='mostrar']");
-    mostrarVista.addEventListener('click',e=>{
+document.addEventListener("DOMContentLoaded", event => {
+   
+    let alumnos = document.getElementById("alumnos"); 
+    let docentes = document.getElementById("docentes"); 
+
+
+    alumnos.addEventListener("click", e=>{
         e.stopPropagation();
+        let vista = "alumnos";
+        colocarVista(vista);
 
-        let modulo = e.srcElement.dataset.modulo;
-        
-        fetch('public/vistas/alumnos/alumnos.html').then( resp=>resp.text() ).then(resp=>{
-            $(`#vistas-${modulo}`).innerHTML = resp;
-            
-            let btnCerrar = $(".close");
-            btnCerrar.addEventListener("click",event=>{
-                $(`#vistas-${modulo}`).innerHTML = "";
-            });
-
-            let cuerpo = $("body"),
-                script = document.createElement("script");
-            script.src = `public/vistas/${modulo}/${modulo}.js`;
-            cuerpo.appendChild(script);
-        });
     });
-});
+
+    docentes.addEventListener("click", e=>{
+        e.stopPropagation();
+        let vista = "docentes";
+        colocarVista(vista);
+    
+    });
+}); 
+ function colocarVista(vista){
+
+    fetch(`public/vistas/${vista}/${vista}.html`).then( resp => resp.text()).then( resp => {
+
+        document.getElementById(`vistas-${vista}`).innerHTML = resp;
+        let btnCerrar = $(".close");
+
+        btnCerrar.addEventListener("click", event => {
+            $(`#vistas-${vista}`).innerHTML = "";
+        });
+
+        let cuerpo = $("body"), script = document.createElement("script");
+        script.src = `public/vistas/${vista}/${vista}.js`;
+        cuerpo.appendChild(script);
+            
+    });
+
+ }
