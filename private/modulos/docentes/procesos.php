ea568621bbdf1cffbcf5c1ab80a6335f77e63963
blady01 <bladimirhenriquez06@gmail.com>
2020-03-27 16:13:10 -0600

    BLAQDY PERREA

--- private/modulos/docentes/procesos.php
+++ private/modulos/docentes/procesos.php
@@ -1,6 +1,7 @@
 <?php 
-include('../../config/config.php');
+include('../../Config/Config.php');
 $docente = new docente($conexion);
+
 $proceso = '';
 if( isset($_GET['proceso']) && strlen($_GET['proceso'])>0 ){
 	$proceso = $_GET['proceso'];
@@ -21,13 +22,16 @@ class docente{
     }
     private function validar_datos(){
         if( empty($this->datos['codigo']) ){
-            $this->respuesta['msg'] = 'por favor ingrese el codigo del docente';
+            $this->respuesta['msg'] = 'por favor ingrese el Código';
         }
         if( empty($this->datos['nombre']) ){
-            $this->respuesta['msg'] = 'por favor ingrese el nombre del docente';
+            $this->respuesta['msg'] = 'por favor ingrese el Nombre';
         }
         if( empty($this->datos['direccion']) ){
-            $this->respuesta['msg'] = 'por favor ingrese la direccion del docente';
+            $this->respuesta['msg'] = 'por favor ingrese la Direccion';
+        }
+        if( empty($this->datos['dui']) ){
+            $this->respuesta['msg'] = 'por favor ingrese el DUI';
         }
         $this->almacenar_docente();
     }
@@ -35,16 +39,44 @@ class docente{
         if( $this->respuesta['msg']==='correcto' ){
             if( $this->datos['accion']==='nuevo' ){
                 $this->db->consultas('
-                    INSERT INTO docentes (codigo,nombre,direccion,telefono) VALUES(
+                    INSERT INTO docentes (codigo,nombre,direccion,dui,telefono) VALUES(
                         "'. $this->datos['codigo'] .'",
                         "'. $this->datos['nombre'] .'",
                         "'. $this->datos['direccion'] .'",
+                        "'. $this->datos['dui'] .'",
                         "'. $this->datos['telefono'] .'"
                     )
                 ');
                 $this->respuesta['msg'] = 'Registro insertado correctamente';
+            } else if( $this->datos['accion']==='modificar' ){
+                $this->db->consultas('
+                    UPDATE docentes SET
+                        codigo      = "'. $this->datos['codigo'] .'",
+                        nombre      = "'. $this->datos['nombre'] .'",
+                        direccion   = "'. $this->datos['direccion'] .'",
+                        dui         = "'. $this->datos['dui'] .'",
+                        telefono    = "'. $this->datos['telefono'] .'"
+                    WHERE idDocente = "'. $this->datos['idDocente'] .'"
+                ');
+                $this->respuesta['msg'] = 'Registro actualizado correctamente';
             }
         }
+    }
+    public function buscarDocente($valor = ''){
+        $this->db->consultas('
+            select docentes.idDocente, docentes.codigo, docentes.nombre, docentes.direccion, docentes.dui, docentes.telefono
+            from docentes
+            where docentes.codigo like "%'. $valor .'%" or docentes.nombre like "%'. $valor .'%" or docentes.dui like "%'. $valor .'%"
+        ');
+        return $this->respuesta = $this->db->obtener_data();
+    }
+    public function eliminarDocente($idDocente = 0){
+        $this->db->consultas('
+            DELETE docentes
+            FROM docentes
+            WHERE docentes.idDocente="'.$idDocente.'"
+        ');
+        return $this->respuesta['msg'] = 'Registro eliminado correctamente';
     }
 }
 ?>
--- public/js/app.js
+++ public/js/app.js
@@ -1,39 +1,29 @@
 var $ = el => document.querySelector(el);
-document.addEventListener("DOMContentLoaded", event => {
-   
-    let alumnos = document.getElementById("alumnos"); 
-    let docentes = document.getElementById("docentes"); 
+function init() {
+    var $ = el => {
+        return el.match(/^#/) ? document.querySelector(el) : document.querySelectorAll(el);
+    }
+    let mostrarVista = $("[class*='mostrar']");
+    console.log(mostrarVista);
+    mostrarVista.forEach(element => {
+        element.addEventListener('click', e => {
+            e.stopPropagation();
 
+            let modulo = e.srcElement.dataset.modulo,
+                form = e.srcElement.dataset.form;
+            fetch(`public/vistas/${modulo}/${form}.html`).then(resp => resp.text()).then(resp => {
+                $(`#vistas-${form}`).innerHTML = resp;
 
-    alumnos.addEventListener("click", e=>{
-        e.stopPropagation();
-        let vista = "alumnos";
-        colocarVista(vista);
-
-    });
-
-    docentes.addEventListener("click", e=>{
-        e.stopPropagation();
-        let vista = "docentes";
-        colocarVista(vista);
-    
-    });
-}); 
- function colocarVista(vista){
-
-    fetch(`public/vistas/${vista}/${vista}.html`).then( resp => resp.text()).then( resp => {
-
-        document.getElementById(`vistas-${vista}`).innerHTML = resp;
-        let btnCerrar = $(".close");
-
-        btnCerrar.addEventListener("click", event => {
-            $(`#vistas-${vista}`).innerHTML = "";
+                let btnCerrar = $(`#btn-close-${form}`);
+                btnCerrar.addEventListener("click", event => {
+                    $(`#vistas-${form}`).innerHTML = "";
+                });
+                import(`../vistas/${modulo}/${form}.js`).then(module => {
+                    module.modulo();
+                });
+                init();
+            });
         });
-
-        let cuerpo = $("body"), script = document.createElement("script");
-        script.src = `public/vistas/${vista}/${vista}.js`;
-        cuerpo.appendChild(script);
-            
     });
-
- }
+}
+init();
