3aad6b84e19caafe7438ec78eeb5b8c885984e6e
blady01 <bladimirhenriquez06@gmail.com>
2020-04-03 15:56:47 -0600

    Usando jquery jquery iu y vue

--- public/vistas/docentes/buscar-docentes.js
+++ public/vistas/docentes/buscar-docentes.js
@@ -1,56 +1,33 @@
-export function modulo(){
-    var $ = el => document.querySelector(el),
-        frmBuscarDocentes = $("#txtBuscarDocente");
-    frmBuscarDocentes.addEventListener('keyup',e=>{
-        traerDatos(frmBuscarDocentes.value);
-    });
-    let modificarDocente = (docente)=>{
-        $("#frm-docentes").dataset.accion = 'modificar';
-        $("#frm-docentes").dataset.iddocente = docente.idDocente;
-        $("#txtCodigoDocente").value = docente.codigo;
-        $("#txtNombreDocente").value = docente.nombre;
-        $("#txtDireccionDocente").value = docente.direccion;
-        $("#txtDuiDocente").value = docente.dui;
-        $("#txtTelefonoDocente").value = docente.telefono;
-    };
-    let eliminarDocente = (idDocente)=>{
-        fetch(`private/Modulos/docentes/procesos.php?proceso=eliminarDocente&docente=${idDocente}`).then( resp=>resp.json() ).then(resp=>{
-            traerDatos('');
-        });
-    };
-    let traerDatos = (valor)=>{
-        fetch(`private/Modulos/docentes/procesos.php?proceso=buscarDocente&docente=${valor}`).then( resp=>resp.json() ).then(resp=>{
-            let filas = ''
-            resp.forEach(docente => {
-                filas += `
-                    <tr data-iddocente='${docente.idDocente}' data-docente='${ JSON.stringify(docente) }'>
-                        <td>${docente.codigo}</td>
-                        <td>${docente.nombre}</td>
-                        <td>${docente.direccion}</td>
-                        <td>${docente.dui}</td>
-                        <td>${docente.telefono}</td>
-                        <td>
-                            <input type="button" class="btn btn-outline-danger text-white" value="del">
-                        </td>
-                    </tr>
-                `;
+var appBuscarDocentes = new Vue({
+    el:'#frm-buscar-docentes',
+    data:{
+        misdocentes:[],
+        valor:''
+    },
+    methods:{
+        buscarDocente:function(){
+            fetch(`private/modulos/docentes/procesos.php?proceso=buscarDocente&docente=${this.valor}`).then(resp=>resp.json()).then(resp=>{
+                this.misdocentes = resp;
             });
-            $("#tbl-buscar-docentes > tbody").innerHTML = filas;
-            $("#tbl-buscar-docentes > tbody").addEventListener("click",e=>{
-                if( e.srcElement.parentNode.dataset.docente==null ){
+        },
+        modificarDocente:function(docente){
+            appdocente.docente = docente;
+            appdocente.docente.accion = 'modificar';
+        },
+        eliminarDocente:function(idDocente){
 
-                    let confirmacion = confirm("DESEA ELIMINAR ESTE REGISTRO?");
+            let confirmacion = confirm("Se eliminará el registro.");
 
-                    if (confirmacion == true){
-                        eliminarDocente( e.srcElement.parentNode.parentNode.dataset.iddocente );
-                    }
-                    else{}
-                    
-                } else {
-                    modificarDocente( JSON.parse(e.srcElement.parentNode.dataset.docente) );
-                }
-            });
-        });
-    };
-    traerDatos('');
-}
+            if (confirmacion == true){
+                fetch(`private/modulos/docentes/procesos.php?proceso=eliminarDocente&docente=${idDocente}`).then(resp=>resp.json()).then(resp=>{
+                    this.buscarDocente();
+
+                });
+            }
+
+        }
+    },
+    created:function(){
+        this.buscarDocente();
+    }
+});
---
+++ public/vistas/materias/buscar-materias.js
@@ -0,0 +1,31 @@
+var appBuscarMaterias = new Vue({
+    el: '#frm-buscar-materias',
+    data:{
+        mismaterias:[],
+        valor:''
+    },
+    methods:{
+        buscarMateria: function(){
+            fetch(`private/Modulos/materias/procesos.php?proceso=buscarMateria&materia=${this.valor}`).then(resp => resp.json()).then(resp => {
+                this.mismaterias = resp;
+            });
+        },
+        modificarMateria: function(materia){
+            appmateria.materia = materia;
+            appmateria.materia.accion = 'modificar';
+        },
+        eliminarMateria: function(idMateria){
+            var mensaje = confirm("¿Estas seguro de eliminar este registro?");
+            if (mensaje) {
+                alert("¡Se ha eliminado el registro con exito!");
+                fetch(`private/Modulos/materias/procesos.php?proceso=eliminarMateria&materia=${idMateria}`).then(resp => resp.json()).then(resp =>{
+                    this.buscarMateria();
+
+                });
+            }
+        }
+    },
+    created: function() {
+        this.buscarMateria();
+    }
+});
